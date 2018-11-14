d01602afdb0fe901a0933895cd2c144befff6653
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-14 13:43:45 +0100

    Zapoceo controller

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,27 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    private String operand = "";
+    private String result = "0";
+    private String operator = "";
+
+    public Label display;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        display.setText("0");
+    }
+
+    public void three(ActionEvent actionEvent) {
+        operand += "3";
+        display.setText(operand);
+    }
 }
