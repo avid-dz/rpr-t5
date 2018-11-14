38c6b2f1f1d77af461c1219d3fa75a384685ab64
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-14 15:07:13 +0100

    Zapoceo property binding

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -7,21 +8,87 @@ import javafx.scene.control.Label;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class Controller {
 
     private String operand = "";
-    private String result = "0";
+    private SimpleStringProperty result;
     private String operator = "";
 
     public Label display;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        display.setText("0");
+    public Controller() {
+        result = new SimpleStringProperty();
+        result.set("0");
+    }
+
+    public String getResult() {
+        return result.get();
+    }
+
+    public SimpleStringProperty resultProperty() {
+        return result;
     }
 
     public void three(ActionEvent actionEvent) {
         operand += "3";
-        display.setText(operand);
+        result.set(operand);
+    }
+
+    public void two(ActionEvent actionEvent) {
+        operand += "2";
+        result.set(operand);
+    }
+
+    public void one(ActionEvent actionEvent) {
+        operand += "1";
+        result.set(operand);
+    }
+
+    public void six(ActionEvent actionEvent) {
+        operand += "6";
+        result.set(operand);
+    }
+
+    public void five(ActionEvent actionEvent) {
+        operand += "5";
+        result.set(operand);
+    }
+
+    public void four(ActionEvent actionEvent) {
+        operand += "4";
+        result.set(operand);
+    }
+
+    public void nine(ActionEvent actionEvent) {
+        operand += "9";
+        result.set(operand);
+    }
+
+    public void eight(ActionEvent actionEvent) {
+        operand += "8";
+        result.set(operand);
+    }
+
+    public void seven(ActionEvent actionEvent) {
+        operand += "7";
+        result.set(operand);
+    }
+
+    public void nil(ActionEvent actionEvent) {
+        if (!operand.equals("")) {
+            operand += "0";
+            result.set(operand);
+        }
+    }
+
+    public void dot(ActionEvent actionEvent) {
+        if (!operand.contains(".")) operand += ".";
+        if (!operand.equals("")) {
+            result.set(operand);
+        }
+    }
+
+    public void plus(ActionEvent actionEvent) {
+
     }
 }
